f4b8155c56ad11636fff93122a7f7266b8e00dfc
D3t3rm1nat1on <bro.chek@mail.ru>
2020-02-10 09:28:29 +0300

    save commit (переношу бд)

--- PostgreSql_Core/Program.cs
+++ PostgreSql_Core/Program.cs
@@ -1,14 +1,92 @@
 ﻿using System;
-using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using Npgsql;
 
 namespace PostgreSql_Core
 {
     class Program
     {
+        
+        static NpgsqlConnection _connection;
+        static string _connString;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // bool exit = false;
+            // string command;
+            //
+            // while (!exit)
+            // {
+            //     command = Console.ReadLine();
+            //     switch (command)
+            //     {
+            //         case "exit":
+            //             exit = true;
+            //             break;
+            //         case "select":
+            //             var a = from ch in command
+            //                 join ch1 in "XXXect" on ch equals ch1
+            //                 select new {ch = ' '};
+            //             break;
+            //     }
+            // }
+
+            Console.WriteLine();
+
+            // ------------------------- ПОДКЛЮЧЕНИЕ К БД ------------------------- //
+            
+            try
+            {
+                _connString = "Server=localhost;Port=5432;User Id=postgres;Password=12345;Database=exercises";
+                _connection = new NpgsqlConnection(_connString);
+                _connection.Open();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Удачное подключение");
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Не удалось подключиться");
+                return;
+            }
+
+            Console.ForegroundColor = default;
+
+
+            // ------------------------- ВСТАВКА К БД ----------------------------- //
+            
+            Member member = new Member();
+            Insert(member);
+                
+            // -------------------------------------------------------------------- //
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ВЫХОД");
+        }
+        
+        public static void Insert(Member m)
+        {
+            // поменять
+            string sql = "insert into cd.members VALUES (39, 'Surname', 'Firstname', 'Address', 123432, 'telephone', NULL, '2019-07-01 00:00:00')";
+            NpgsqlCommand command = new NpgsqlCommand(sql, _connection);
+            command.ExecuteNonQuery();
+            Console.WriteLine("вставка завершена");
         }
     }
+
+    public class Member
+    {
+        public int MemId { get; set; }
+        public string Surname { get; set; } = "";
+        public string Firstname { get; set; } = "";
+        public string Address { get; set; } = "";
+        public int Zipcode { get; set; } = 0;
+        public string Telephone { get; set; } = "";
+        public int RecommendedBy { get; set; }
+        public DateTime JoinDate { get; set; } = DateTime.Now;
+    }
+    
+    
+
+
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # PostgreSql_Core
-Для практики PostgreSQL в .Net Core
+Для практики PostgreSQL в .Net Core  
+Использую пакет Npgsql для работы с БД  
+БД взята с pgexercises.com  
 ![image](https://user-images.githubusercontent.com/42013240/74113685-dbae8480-4bb6-11ea-8694-476bcc9b4434.png)
