b743aba30c0dbc780d2048385885f8a63af8542b
D3t3rm1nat1on <bro.chek@mail.ru>
2020-02-10 03:35:23 +0300

    Initial commit

---
+++ PostgreSql_Core/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace PostgreSql_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
